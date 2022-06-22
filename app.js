313685a02e87c2fdf3b1f948a27974ea1e9c60b7
Jason Garcia <jaygarcia0530@gmail.com>
2022-06-22 16:43:20 -0700

    Test

---
+++ app.js
@@ -0,0 +1,48 @@
+const bodyParser = require('body-parser');
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+
+app.use(express.static('.'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const db = new sqlite3.Database(`:memory:`);
+db.serialize(function () {
+ db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
+ db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
+});
+
+
+app.get('/', (req,res)=>{
+    res.sendFile('index.html');
+})
+
+app.post('/login', (req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    const query = `SELECT title FROM user where username = '${username}' AND password = '${password}'`;
+    console.log(`username: ${username} password: ${password}`)
+    console.log(`query: ${query}`)
+    console.log('Route hit')
+
+    db.get(query, function (err, row) {
+
+		if (err) {
+			console.log('ERROR', err);
+			res.redirect("/index.html#error");
+		} else if (!row) {
+			res.redirect("/index.html#unauthorized");
+		} else {
+			res.send('Hello <b>' + row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>');
+		}
+	});
+    
+
+})
+
+app.listen(3001, ()=>{
+    console.log('Server listening on port 3001');
+})
